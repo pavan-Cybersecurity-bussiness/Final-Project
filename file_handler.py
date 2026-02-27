831d5e18b31ffc3a1e3bb0f72ab993a359e3fa0a
PAVAN-A-K <ashokkumar.krishnaveni.pavan@gmail.com>
2026-02-27 15:25:44 +0530

    first commit

---
+++ file_handler.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+FILE_NAME = "tasks.txt"
+
+
+def load_tasks():
+    pass
+
+
+def save_tasks(tasks):
+    pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+import json
+import os
